ba7e691d3d385934a095bdc83a9a2d7e0661f987
karthi050197 <157946841+karthi050197@users.noreply.github.com>
2024-06-25 19:01:18 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-docker login -u karthick0501  -p dckr_pat_8gbfGPimLBn6h3h-X-6zhoJS0U0
-
 if [[ $GIT_BRANCH == "origin/dev" ]]; then
-       sh 'chmod +x build.sh'
-       sh './build.sh'
+    # Build your project
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker login -u karthick0501 -p dckr_pat_8gbfGPimLBn6h3h-X-6zhoJS0U0
+    docker tag devops-build karthick0501/dev
+    docker push karthick0501/dev
 
-        docker tag devops-build karthick0501/dev
-        docker push karthick0501/dev
-
-if [[ $GIT_BRANCH == "origin/master" ]]; then
-        sh 'chmod +x build.sh'
-        sh './build.sh'
-
-        docker tag devops-build karthick0501/prod
-        docker push karthick0501/prod
-
-else
-        echo "failed"
+elif [[ $GIT_BRANCH == "origin/master" ]]; then
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker login -u karthick0501 -p dckr_pat_8gbfGPimLBn6h3h-X-6zhoJS0U0
+    docker tag devops-build karthick0501/prod 
+    docker push karthick0501/prod
 fi
