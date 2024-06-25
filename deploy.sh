fe8666d2f6deee58304502b28329cd1063fca728
DreJones06 <157946805+DreJones06@users.noreply.github.com>
2024-06-26 00:14:16 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-if [[ $GIT_BRANCH == "origin/Dev" ]]; then
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
     # Build your project
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u karthick0501 -p dckr_pat_8gbfGPimLBn6h3h-X-6zhoJS0U0
-    docker tag devops-build karthick0501/dev
-    docker push karthick0501/dev
+    docker login -u drejones06 -p dckr_pat_wDWAxLoonygIJlzcpF75wsWL_d4
+    docker tag devops-build drejones06/dev
+    docker push drejones06/dev
 
 elif [[ $GIT_BRANCH == "origin/master" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u karthick0501 -p dckr_pat_8gbfGPimLBn6h3h-X-6zhoJS0U0
-    docker tag devops-build karthick0501/prod 
-    docker push karthick0501/prod
+    docker login -u drejones06 -p dckr_pat_wDWAxLoonygIJlzcpF75wsWL_d4
+    docker tag devops-build drejones06/prod 
+    docker push drejones06/prod
 else
     echo "Failed"
 fi
