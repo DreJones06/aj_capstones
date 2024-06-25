8300a1673077bb42f6adf1d7f20efca7c4a975d9
karthi050197 <157946841+karthi050197@users.noreply.github.com>
2024-06-25 15:47:57 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+docker login -u karthi050197  -p dckr_pat_8gbfGPimLBn6h3h-X-6zhoJS0U0
 
-docker-compose up -d
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+       sh 'chmod +x build.sh'
+       sh './build.sh'
 
-# Check if the deployment was successful
-if [ $? -eq 0 ]; then
-    echo "Docker image deployed successfully."
+        docker tag devops-build karthi050197/dev
+        docker push karthi050197/dev
+
+if [[ $GIT_BRANCH == "origin/master" ]]; then
+        sh 'chmod +x build.sh'
+        sh './build.sh'
+
+        docker tag devops-build karthi050197/prod
+        docker push karthi050197/prod
+
 else
-    echo "Failed to deploy Docker image."
-    exit 1
+        echo "failed"
 fi
